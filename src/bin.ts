2d0e8a10acb07145f12243aec868d195c9cba3dc
Blake Embrey <hello@blakeembrey.com>
2018-12-15 11:58:18 -0800

    Add `--help` command to bin script

--- src/bin.ts
+++ src/bin.ts
@@ -1,24 +1,43 @@
 #!/usr/bin/env node
 
+import arg = require('arg')
 import { join } from 'path'
 import { createServer } from 'http'
 import { createHandler } from 'servie-http'
 import { proxy } from './index'
 
-const args = require('arg')({
+const args = arg({
   '--config': String,
   '--port': Number,
   '--hostname': String,
+  '--help': Boolean,
   '-c': '--config',
   '-p': '--port',
+  '-h': '--help',
   '-H': '--hostname'
 })
 
 const {
+  '--help': help,
   '--config': config,
   '--port': port = 9000,
   '--hostname': hostname = '0.0.0.0'
 } = args
+
+if (help) {
+  console.log(`
+  Description
+    Start a HTTP-based micro-service proxy
+  Usage
+    $ microservice-proxy -p <port> -c <config.json>
+  Options
+    --config, -c    The route configuration file
+    --port, -p      The port number to start the proxy (${port})
+    --hostname, -H  The host name to start the proxy (${hostname})
+  `)
+
+  process.exit(0)
+}
 
 if (!config) {
   console.error('> Start with `--config` (`-c`) option')

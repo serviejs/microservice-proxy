021b2c7aacb586990e9e445ca3d6ae8de997bfca
Blake Embrey <hello@blakeembrey.com>
2019-11-13 13:10:09 +1100

    Add support for rewriting paths via `newPath`

--- src/index.spec.ts
+++ src/index.spec.ts
@@ -1,9 +1,116 @@
 import { proxy } from "./index";
+import { Response, Request } from "servie/dist/node";
 
 describe("microservice proxy", () => {
-  it("should create middleware", () => {
-    const m = proxy([]);
+  it("should create a proxy", async () => {
+    const send = jest.fn(async (_: Request) => new Response("test"));
 
-    expect(typeof m).toBe("function");
+    const m = proxy(
+      [
+        {
+          url: "http://example.com"
+        }
+      ],
+      send
+    );
+
+    const res = await m(new Request("http://localhost"), jest.fn());
+
+    expect(send).toHaveBeenCalledTimes(1);
+    expect(send.mock.calls[0][0].url).toEqual("http://example.com/");
+    expect(await res.text()).toEqual("test");
+  });
+
+  it("should map to a new path", async () => {
+    const send = jest.fn(async (_: Request) => new Response("test"));
+
+    const m = proxy(
+      [
+        {
+          url: "http://example.com",
+          newPath: "/test"
+        }
+      ],
+      send
+    );
+
+    const res = await m(new Request("http://localhost"), jest.fn());
+
+    expect(send).toHaveBeenCalledTimes(1);
+    expect(send.mock.calls[0][0].url).toEqual("http://example.com/test");
+    expect(await res.text()).toEqual("test");
+  });
+
+  it("should return `next` when route is not matched", async () => {
+    const send = jest.fn(async (_: Request) => new Response("test"));
+    const next = jest.fn(async () => new Response("404"));
+
+    const m = proxy(
+      [
+        {
+          url: "http://example.com",
+          path: "/foo"
+        }
+      ],
+      send
+    );
+
+    const res = await m(new Request("http://localhost"), next);
+
+    expect(next).toHaveBeenCalledTimes(1);
+    expect(await res.text()).toEqual("404");
+  });
+
+  it("should match specified methods", async () => {
+    const send = jest.fn(async (_: Request) => new Response("test"));
+    const next = jest.fn(async () => new Response("404"));
+
+    const m = proxy(
+      [
+        {
+          url: "http://example.com",
+          methods: ["GET"]
+        }
+      ],
+      send
+    );
+
+    const res = await m(new Request("http://localhost"), next);
+
+    expect(next).toHaveBeenCalledTimes(0);
+    expect(send).toHaveBeenCalledTimes(1);
+    expect(await res.text()).toEqual("test");
+
+    const res2 = await m(
+      new Request("http://localhost", { method: "POST" }),
+      next
+    );
+
+    expect(next).toHaveBeenCalledTimes(1);
+    expect(send).toHaveBeenCalledTimes(1);
+    expect(await res2.text()).toEqual("404");
+  });
+
+  it("should rewrite paths", async () => {
+    const send = jest.fn(async (req: Request) => new Response(req.url));
+
+    const m = proxy(
+      [
+        {
+          url: "http://example.com",
+          path: "/:id",
+          newPath: "/test/$1"
+        }
+      ],
+      send
+    );
+
+    const res = await m(
+      new Request("http://localhost/123?test=true"),
+      jest.fn()
+    );
+
+    expect(send).toHaveBeenCalledTimes(1);
+    expect(await res.text()).toEqual("http://example.com/test/123?test=true");
   });
 });
--- src/index.ts
+++ src/index.ts
@@ -2,63 +2,83 @@ import { Request, Response } from "servie/dist/node";
 import { useRawBody } from "servie/dist/common";
 import { pathToRegexp } from "path-to-regexp";
 import { getURL } from "servie-url";
-import { transport } from "popsicle-transport-http";
+import { transport, HttpResponse } from "popsicle-transport-http";
+import { URL } from "url";
 
 /**
  * Proxy configuration object.
  */
 export interface Config {
+  url: string;
   path?: string;
   methods?: string[];
-  url: string;
+  newPath?: string;
+  end?: boolean;
+  start?: boolean;
+  sensitive?: boolean;
 }
 
 /**
  * Create a simple service to proxy requests.
  */
 export class Service {
-  re?: RegExp;
-  methods?: Set<string>;
   url: string;
+  re: RegExp;
+  methods: Set<string>;
+  newPath?: string;
 
   constructor(config: Config) {
-    this.re = config.path
-      ? pathToRegexp(config.path, undefined, { strict: true })
-      : undefined;
-    this.methods = config.methods
-      ? new Set(config.methods.map(x => x.toLowerCase()))
-      : undefined;
-    this.url = config.url;
+    const { path, methods, url, newPath, end, start, sensitive } = config;
+
+    this.re = pathToRegexp(path || "/", undefined, {
+      strict: true,
+      start,
+      end,
+      sensitive
+    });
+    this.methods = new Set((methods || []).map(x => x.toLowerCase()));
+    this.url = url;
+    this.newPath = newPath;
   }
 
   isMethod(method: string) {
-    return this.methods ? this.methods.has(method.toLowerCase()) : true;
+    return this.methods.size === 0 || this.methods.has(method.toLowerCase());
   }
 
   isPath(path: string) {
-    return this.re ? this.re.test(path) : true;
+    return this.re.test(path);
   }
 
-  matches(req: Request) {
-    return (
-      this.isMethod(req.method) && this.isPath(getURL(req).pathname || "/")
-    );
+  matches(url: URL, method: string) {
+    return this.isMethod(method) && this.isPath(url.pathname);
+  }
+
+  getPath(path: string) {
+    if (!this.newPath) return path;
+
+    return path.replace(this.re, this.newPath);
+  }
+
+  getUrl(url: URL) {
+    return `${this.url}${this.getPath(url.pathname)}${url.search}${url.hash}`;
   }
 }
 
 /**
  * Proxy any number of paths to services.
  */
-export function proxy(configs: Config[]) {
+export function proxy(
+  configs: Config[],
+  send: (req: Request, next: () => never) => Promise<Response> = transport()
+) {
   const services = configs.map(config => new Service(config));
 
-  // Forward HTTP requests raw without any pre-processing.
-  const send = transport();
-
   return async (req: Request, next: () => Promise<Response>) => {
+    const url = getURL(req);
+
     for (const service of services) {
-      if (service.matches(req)) {
-        const proxyReq = new Request(`${service.url}${req.url}`, {
+      if (service.matches(url, req.method)) {
+        const proxyReq = new Request(service.getUrl(url), {
           method: req.method,
           headers: req.headers,
           signal: req.signal,
@@ -68,7 +88,7 @@ export function proxy(configs: Config[]) {
         });
 
         return send(proxyReq, () => {
-          return Promise.reject(new TypeError("Unhandled proxy request"));
+          throw new TypeError("Unhandled proxy request");
         });
       }
     }
